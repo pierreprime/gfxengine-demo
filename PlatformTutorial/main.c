be4db7579dbd3884849463cf8cd5ee01f0eade10
unknown <pierre.vermeil@gmail.com>
2018-08-09 23:19:57 +0200

    Started ep.22 tileset scrolling gfx

--- PlatformTutorial/main.c
+++ PlatformTutorial/main.c
@@ -4,28 +4,31 @@
 #include "myDefines.h"
 
 const int map1[300] = {
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 3, 4, 5, 2, 2, 3, 4, 5, 2,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-}
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+2, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 3, 4, 5, 2, 2, 3, 4, 5, 2,
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+};
 
 int ram_map[300];
 
 int main(int argc, char *argv[])
 {
-    int i;
+    // this part does not change with gfxengine
+    int i, x, y, tile_value;
+    int image_nb;
+    int sprnb_pointer;
 
     printf("\nTUTO PLATEFORME");
 
@@ -36,15 +39,41 @@ int main(int argc, char *argv[])
         ram_map[i] = map1[i];
 
     loadImageTra("cd/gfx/tileset1.png", IMAGE_TILES, 64, 64);
+    loadImageTra("cd/gfx/spritesheet.png", IMAGE_SPRITE, 128, 128);
+    //loadImageTra("cd/gfx/stone_background.png", 3, 512, 512);
 
+    getImageBankTra(IB_POINTER, IMAGE_SPRITE, 0, 0, 16, 16);
+    sprnb_pointer = spr_initFreeSprite(0,0,0,IB_POINTER);
+    //
+
+    image_nb = 1;
     // Commence à l'ImageBank 30 pour la tile 0 (non dessiné)
     for(y=0; y<64; y+=16)
     {
-        getImageBankTra()
+        for(x=0; x<64; x+=16)
+        {
+            getImageTile(image_nb, IMAGE_TILES, x, y, 16, 16);
+            image_nb++;
+        }
     }
+
+    // 0 mapNb, 20 width in tiles, 15 width in tiles, 16 tile size px, ram_map
+    sm_setScrollData(0, 20, 15, 16, ram_map);
+    sm_setScrollView(0, 0.0f, 0.0f);
 
     while(1)
     {
+        prepareEcritureVideoGlobale();
+        prepareEcritureVideoTra();
+
+        // upside down and left to right tiles display
+        //blitImageTra(3,0,0);
+        sm_scrollDraw(0,0,0);
+        spr_showAllSprites();
+
+        termineEcritureVideoTra();
+        termineEcritureVideoGlobale();
+
         checkController();
     }
 
--- PlatformTutorial/myDefines.h
+++ PlatformTutorial/myDefines.h
@@ -1,22 +1,12 @@
 #ifndef MYDEFINES_H
 #define MYDEFINES_H
 
-#define IMAGE_DECOR 0
+#define IMAGE_TILES 0
 #define IMAGE_SPRITE 1
-#define IMAGE_FONT 2
 
-#define IB_ALIEN_F0 0
-#define IB_ALIEN_F1 1
-#define IB_PLAYER_F0 2
-#define IB_SHOOT_ALIEN_F0 3
-#define IB_SHOOT_PLAYER_F0 4
-#define IB_EXPLODE_F0 5
-#define IB_EXPLODE_F1 6
-#define IB_EXPLODE_F2 7
-#define IB_EXPLODE_F3 8
-#define IB_EXPLODE_F4 9
-#define IB_EXPLODE_F5 10
+#define SPRITE0 0
+#define SCROLL0 0
 
-#define PL_COOLDOWN 25
+#define IB_POINTER 0
 
 #endif
