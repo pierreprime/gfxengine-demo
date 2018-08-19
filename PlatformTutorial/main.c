0dc84354c0d3754a8db819a146478e7c3315cf41
unknown <pierre.vermeil@gmail.com>
2018-08-20 00:52:55 +0200

    episode 22 finished tiles collisions

--- PlatformTutorial/main.c
+++ PlatformTutorial/main.c
@@ -4,24 +4,14 @@
 #include "myDefines.h"
 
 const int map1[300] = {
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-2, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 3, 4, 5, 2, 2, 3, 4, 5, 2,
-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 3, 4, 5, 2, 2, 2, 1, 2, 2, 2, 2, 3, 4, 5, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 };
 
-int ram_map[300];
+const int map1_collision[300] = {
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6
+};
+
+
 
 int main(int argc, char *argv[])
 {
@@ -29,6 +19,11 @@ int main(int argc, char *argv[])
     int i, x, y, tile_value;
     int image_nb;
     int sprnb_pointer;
+    int xSprite, ySprite, xTile, yTile;
+    int tile_attribute;
+
+    int ram_map[300];
+int ram_map_collision[300];
 
     printf("\nTUTO PLATEFORME");
 
@@ -37,6 +32,8 @@ int main(int argc, char *argv[])
 
     for(i=0; i<300; i++)
         ram_map[i] = map1[i];
+    for(i=0; i<300; i++)
+        ram_map_collision[i] = map1_collision[i];
 
     loadImageTra("cd/gfx/tileset1.png", IMAGE_TILES, 64, 64);
     loadImageTra("cd/gfx/spritesheet.png", IMAGE_SPRITE, 128, 128);
@@ -53,13 +50,20 @@ int main(int argc, char *argv[])
         for(x=0; x<64; x+=16)
         {
             getImageTile(image_nb, IMAGE_TILES, x, y, 16, 16);
+            tm_setTileDef(image_nb, TILEATTR_NULL, TILEATTR_NULL, TILEATTR_NULL);
             image_nb++;
         }
     }
 
+    tm_setTileDef(1, TILEATTR_PLATFORM, TILEATTR_NULL, TILEATTR_NULL);
+    tm_setTileDef(6, TILEATTR_PLATFORM, TILEATTR_NULL, TILEATTR_NULL);
+
     // 0 mapNb, 20 width in tiles, 15 width in tiles, 16 tile size px, ram_map
     sm_setScrollData(0, 20, 15, 16, ram_map);
     sm_setScrollView(0, 0.0f, 0.0f);
+
+    sm_setScrollData(SCROLL1, 20, 15, 16, ram_map_collision);
+    sm_setScrollView(SCROLL1, 0.0f, 0.0f);
 
     while(1)
     {
@@ -75,6 +79,35 @@ int main(int argc, char *argv[])
         termineEcritureVideoGlobale();
 
         checkController();
+
+        // make cursor move
+        if(getKeyUp())
+            spr_moveSpriteRelative(sprnb_pointer, 0.0f, -1.0f);
+        if(getKeyDown())
+           spr_moveSpriteRelative(sprnb_pointer, 0.0f, 1.0f);
+        if(getKeyLeft())
+            spr_moveSpriteRelative(sprnb_pointer, -1.0f, 0.0f);
+        if(getKeyRight())
+            spr_moveSpriteRelative(sprnb_pointer, 1.0f, 0.0f);
+
+        xSprite = (int)spr_getSpriteX(sprnb_pointer);
+        ySprite = (int)spr_getSpriteY(sprnb_pointer);
+
+        // coordinates of tile where we are
+        xTile = xSprite / 16;
+        yTile = ySprite / 16;
+
+        // tile map array value formula
+        //tile_value = ram_map[xTile + (yTile * 20)];
+        // tile value gfx version
+        // determines what is displayed tile
+        tile_value = sm_getTileFromScrollMap(SCROLL1, xTile, yTile);
+        tile_attribute = tm_getTileDefv1(tile_value);
+
+        printf("\n Sprite x : %d, y : %d", xSprite, ySprite);
+        printf("\n Tile x : %d, y : %d", xTile, yTile);
+        printf("\n Tile value : %d", tile_value);
+        printf("\n Tile attribute : %d", tile_attribute);
     }
 
     return 0;
--- PlatformTutorial/myDefines.h
+++ PlatformTutorial/myDefines.h
@@ -6,7 +6,11 @@
 
 #define SPRITE0 0
 #define SCROLL0 0
+#define SCROLL1 1
 
 #define IB_POINTER 0
+
+#define TILEATTR_NULL 0
+#define TILEATTR_PLATFORM 1
 
 #endif
